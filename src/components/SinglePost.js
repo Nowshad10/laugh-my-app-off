297c1d754b074659dfd683da48bad1861c9649a7
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-24 16:50:33 +0100

    Added delete post functionality

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,10 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from './useFetch';
+
 
 const SinglePost = () => {
 
     const { id } = useParams();
     const { data: post, error, isLoading } = useFetch(`http://localhost:8000/posts/${id}`);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/posts/${id}`, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            navigate('/');
+        })
+    };
 
     return (
         <div className="post-details">
@@ -15,6 +26,7 @@ const SinglePost = () => {
                     <h2>{ post.title }</h2>
                     <p>Written by { post.author }</p>
                     <div>{ post.body }</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
